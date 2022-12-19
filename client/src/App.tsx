9f5a42a63b4c5ef312338181d210e7c8402e8512
Canh <pcanhgm@gmail.com>
2022-12-19 18:14:58 +0700

    init ts pj

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+function App() {
+  return (
+   <Container>
+    APP
+   </Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+  
+`
